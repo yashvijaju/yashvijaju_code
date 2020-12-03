f21f1da85048da814a78d3891f43fea654bf5436
yjaju <yjaju@usc.edu>
2020-12-03 16:57:38 +0530

    mettaCap budda

--- pages/art/circle-of-light.js
+++ pages/art/circle-of-light.js
@@ -1,57 +1,127 @@
 import React from 'react';
-import { Grid, Typography, Hidden, Tooltip } from '@material-ui/core';
+import { Grid, Divider, Typography, Hidden, Tooltip } from '@material-ui/core';
+import { makeStyles } from '@material-ui/core/styles'
+// Components
+import { Cover } from '../../components/Projects/Cover'
+import { Intro } from '../../components/Projects/Intro'
+import { ProblemStatement } from '../../components/Projects/Problem_Statement'
 
-const font = 'Segoe UI';
+const primary = '#9E0D1B';
+const primary_25 = '#E6C2C6';
+const secondary = '#F23838';
+const tertiary = '#FFDCD0';
+
+const useStyles = makeStyles((theme) => ({
+    container: {
+        padding: '0 5vw',
+    },
+    divider: {
+        margin: '5vh 5vw 7.5vh',
+        display: 'flex',
+        width: '90vw',
+        background: primary_25,
+        [theme.breakpoints.down('xs')]: {
+            margin: '5vh 5vw 5vh',
+        },
+    },
+    image: {
+        border: '1px solid #9E0D1B',
+        height: 'auto',
+        width: '100%',
+        margin: '2vh 0',
+    },
+    circle: {
+        width: '50pt',
+        height: '50pt',
+        borderRadius: '50%',
+        backgroundColor: tertiary,
+        display: 'inline-flex',
+        zIndex: '-1',
+        [theme.breakpoints.down('xs')]: {
+            width: '30pt',
+            height: '30pt',
+        },
+    },
+    title: {
+        display: 'inline-block',
+        margin: '0 0 0 -30pt',
+        zIndex: '1',
+        color: secondary,
+        [theme.breakpoints.down('xs')]: {
+            margin: '0 0 0 -20pt',
+        },
+    },
+    text: {
+        color: primary,
+        margin: '2vh 0',
+    }
+}));
+
+function ContentItem(props) {
+    const classes = useStyles();
+
+    return(
+        <>
+        <Grid item xs={12} container direction="row" alignItems="center">
+            <div className={classes.circle}></div>
+            <Hidden xsDown>
+                <Typography variant="h3" className={classes.title}>{props.title}</Typography>
+            </Hidden>
+            <Hidden smUp>
+                <Typography variant="h5" className={classes.title}>{props.title}</Typography>
+            </Hidden>
+        </Grid>
+        <Grid item xs={12} container direction="column">
+            <Typography variant="body2" className={classes.text}>
+                {props.text}
+            </Typography>
+            <Grid container direction="row">
+                {props.images}
+            </Grid>
+        </Grid>
+        </>
+    )
+}
+
+function ContentItemText(props) {
+    const classes = useStyles();
+
+    return(
+        <>
+        <Grid item xs={12}>
+            <Typography variant="body2" className={classes.text}>
+                {props.text}
+            </Typography>
+        </Grid>
+        <Grid item xs={12} container direction="row" justify="space-between">
+            {props.images}
+        </Grid>
+        </>
+    )
+}
 
 
 export default function Story() {
-    
-    function handleBuddhaMethod() {
-        var temp = document.getElementById("BuddhaMethodology");
-        if (temp.style.display === 'none') {
-        temp.style.display = 'inline';
-        }
-        else {
-        temp.style.display = 'none';
-        }
-    }
+    const classes = useStyles();
     
     return(
-        <div>
-            <Grid container direction="row" justify="center" style={{backgroundColor: 'white', border: '5px solid #efefef', outline: 'none', width: '70vw', 'maxHeight': '75vh', 'overflowY': 'auto', alignItems: 'center', 'paddingBottom': '5vh', 'paddingTop': '5vh'}}>
-                <Grid container direction="row" justify="center">
-                    <Grid item xs={10} md={5} style={{marginTop: '1vh'}}>
-                    <Grid container direction="column" justify="center" style={{alignItems: 'center'}}>
-                        <img id="Gautam Buddha" src="/assets/buddh.jpg" alt="Gautam Buddha" title="Gautam Buddha" style={{'width': '90%'}}/>
-                        <img id="Gautam Buddha" src="/assets/buddhaeyes.jpg" alt="Gautam Buddha" title="Gautam Buddha" style={{'width': '90%', marginTop: '5px'}}/>
-                    </Grid>
-                    </Grid>
-                    <Grid item xs={10} md={6} style={{marginTop: '1vh', display: 'flex', flexDirection:'column', justifyContent: 'center', alignItems: 'left'}}>
-                    <Typography id="story-modal" variant="caption" style={{fontFamily: font}}>
-                        'Circle of Light' is a 43-by-39 inch oil portrait of the Gautam Buddha. The Guatam Buddha is an important figure in Hindu mythology, famous for his surrender of the materialistic world in pursuit of complete enlightenment and self-actualisation. His ideology and way-of-life attracts hundreds of thousands of followers. This painting is inspired by another painting I had seen in a book. <br/> 
-                        <br/>
-                        In the portrait, Buddha’s facial features are softened and symmetricalised as a manifestation of his ideology attracting a multitude of followers, as in accordance with the Theory of Evolutionary Advantage. His eyelids are painted half-open to radiate calmness and serenity, as achieved through meditation. This also allows observers to feel as if the Buddha is looking right at them regardless of where they position themselves and thus suggesting a quality of being omniscient. Around Buddha’s head, an aura has been painted to illustrate the high pedestal followers place him on. Moreover, given the asssociation of the colors blue and white with the ocean and sky, shades of these hues have been used throughout the portrait to emphasise the theme of embracing the natural. <br/> 
-                        <br/>
-                        A bright “Circle of Light” illustrates the Buddha's heart to visually define his state of complete internal enlightenment. Just as he preaches to his pupils in order to share his knowledge, tendrils of this “Circle of Light” extend from his soul and merge with his surroundings. The color palette of yellow, orange, and red accentuates the portrayal of Buddha as a beacon of hope and fresh beginnings. 
-                        <br/> <br/>
-                    </Typography>
-                    <Typography variant="caption" style={{fontFamily: font}} onClick={()=>handleBuddhaMethod()}>
-                        Methodology: (click to toggle show/ hide)<br/>
-                    </Typography>
-                    <Typography id="BuddhaMethodology" variant="caption" style={{fontFamily: font, display:'none'}}>
-                        As the canvas was large, I started by pencilling out a 10*10 grid - this allowed me to later maintain symmetry while sketching the portrait. I then drew a rough pencil sketch of the Buddha as this was the main object of my painting. Then, to even the tone and decrease the absorption of the oil paint by the canvas, I painted the entire canvas with a thick white base coat to prepare my canvas for the firs coat. I started with the background, using a large brush to paint the top corners in a deep, royal blue. Then, I used a slightly thinner brush to create the aura around Buddha’s head, rotating the brush in a circular motion as I started from the centre and moved to the corners, decreasing the pressure exerted on the brush to paint the different hues of blue as the palette moved from light blue towards the centre to dark blue in the extreme corners. After this, I had to wait for a few days to allow the paint to semi-dry before I began detailing Buddha’s facial features. As this was the first coat of oil paints out of three, my goal was to outline the more general features rather than expending energy focusing on the inticrate details. I first painted the Buddha's hair given this was a dark color; this was done to prevent it from smudging into the lighter shades used while painting his forehead. I then painted the forehead, cheekbones, and ears in a slightly lighter shade of blue, using paints of red and white to create highlights and tone. Once this was done, I painted the Buddha’s neck in a darker blue so as to greatly accentuate the “circle of light” in his chest - represented in the bright shades of yellow, orange, red, and green. Thus, my first coat was complete. 
-                        <br/> <br/> 
-                        Before starting my second coat, I had to wait for the first coat of paint to dry. However, this time, I had to wait for longer (about two-three weeks) to ensure thorough drying as I was to resume painting from the very top, and didn’t want to smudge the lower half while doing so. The primary purpose behind the second coat was to perfect all the background illustrations. It hence focused on detailed shading to create smooth transitions from one hue to another without allowing individual brush strokes to show. This time, I detailed Buddha’s facial features as well, creating the effect of light coming from within by highlighting his central facial section. And thus, with the completion of the second coat, I perfected my painting’s background. The process of waiting continued once again. I then began work on my third coat, focused on perfecting and finely detailing the portrait’s main object - the Buddha’s face. This was by far the most difficult aspect of the painting - especially the eyes - as I had to be doubly careful to ensure that the features radiated serenity while also ensuring that my brush strokes did, at no point, touch the surrounding background. Only after painting this layer and allowing it to dry completely was I able to add the final finishing touches to the painting. I ensured that not only was the border between the Buddha and the background sharp, but also painted the sides of the canvas with a dark bluish-black paint to cover any strokes that went astray down the sides. To mark my painting’s completion, I then used a thin brush to paint my name in the right-hand corner in white. <br/>
-                        <br/>
-                    </Typography>
-                    <Typography variant="caption" style={{fontFamily: font}}>
-                        Painted: May 2014 <br/>
-                        <br/>
-                    </Typography>
-                    
-                    </Grid>
-                </Grid>
-            </Grid>
-        </div>
+        <>
+        <Cover project_name="Circle of Light : Gautam Buddha" bg_color={tertiary} title_color={secondary}></Cover>
+        <Grid container className={classes.container}>
+            <Intro title_color={primary_25} text_color={primary} company={"N/A"} role={"Artist"} year={"2014"} category={<div>Traditional Art <br/> <br/></div>} team={"Independent Project"} tools={<div><u>Art Supplies:</u> Oil Paints, Canvas, Frame</div>} tools1={<div><u>Art Supplies:</u> <br/>Oil Paints <br/>Canvas <br/>Frame <br/></div>} tools2={<div></div>} introduction={<div>Circle of Light' is a 43-by-39 inch oil portrait of the Gautam Buddha. The Guatam Buddha is an important figure in Hindu mythology, famous for his surrender of the materialistic world in pursuit of complete enlightenment and self-actualisation. His ideology and way-of-life attracts hundreds of thousands of followers. <br/> This painting is inspired by another artist's work.</div>}/>
+        </Grid>
+
+        <Divider className={classes.divider}/>
+
+        <Grid container direction="column" className={classes.container}>
+            <ContentItem title="about the painting" text={<div>'Circle of Light' is a 43-by-39 inch oil portrait of the Gautam Buddha. The Guatam Buddha is an important figure in Hindu mythology, famous for his surrender of the materialistic world in pursuit of complete enlightenment and self-actualisation. His ideology and way-of-life attracts hundreds of thousands of followers. This painting is inspired by another painting I had seen in a book. <br/> <br/>In the portrait, Buddha’s facial features are softened and symmetricalised as a manifestation of his ideology attracting a multitude of followers, as in accordance with the Theory of Evolutionary Advantage. His eyelids are painted half-open to radiate calmness and serenity, as achieved through meditation. This also allows observers to feel as if the Buddha is looking right at them regardless of where they position themselves and thus suggesting a quality of being omniscient. Around Buddha’s head, an aura has been painted to illustrate the high pedestal followers place him on. Moreover, given the asssociation of the colors blue and white with the ocean and sky, shades of these hues have been used throughout the portrait to emphasise the theme of embracing the natural. <br/><br/>A bright “Circle of Light” illustrates the Buddha's heart to visually define his state of complete internal enlightenment. Just as he preaches to his pupils in order to share his knowledge, tendrils of this “Circle of Light” extend from his soul and merge with his surroundings. The color palette of yellow, orange, and red accentuates the portrayal of Buddha as a beacon of hope and fresh beginnings.</div>} images={<><img className={classes.image} src="/assets/projects/circle-of-light/circle-of-light.jpg" style={{padding: '0 0 1vh 0'}}/></>}/>
+
+            <ContentItem title="methodology" text={<div>As the canvas was large, I started by pencilling out a 10*10 grid - this allowed me to later maintain symmetry while sketching the portrait. I then drew a rough pencil sketch of the Buddha as this was the main object of my painting. Then, to even the tone and decrease the absorption of the oil paint by the canvas, I painted the entire canvas with a thick white base coat to prepare my canvas for the first coat. <br/><br/>I started with the background, using a large brush to paint the top corners in a deep, royal blue. Then, I used a slightly thinner brush to create the aura around Buddha’s head, rotating the brush in a circular motion as I started from the centre and moved to the corners, decreasing the pressure exerted on the brush to paint the different hues of blue as the palette moved from light blue towards the centre to dark blue in the extreme corners. <br/><br/>After this, I had to wait for a few days to allow the paint to semi-dry before I began detailing Buddha’s facial features. As this was the first coat of oil paints out of three, my goal was to outline the more general features rather than expending energy focusing on the inticrate details. I first painted the Buddha's hair given this was a dark color; this was done to prevent it from smudging into the lighter shades used while painting his forehead. I then painted the forehead, cheekbones, and ears in a slightly lighter shade of blue, using paints of red and white to create highlights and tone. <br/><br/>Once this was done, I painted the Buddha’s neck in a darker blue so as to greatly accentuate the “circle of light” in his chest - represented in the bright shades of yellow, orange, red, and green. Thus, my first coat was complete.</div>} images={<><img className={classes.image} src="/assets/projects/circle-of-light/circle-of-light-eyes.jpg"/></>}/>
+
+            <ContentItemText text={<div>Before starting my second coat, I had to wait for the first coat of paint to dry. However, this time, I had to wait for longer (about two-three weeks) to ensure thorough drying as I was to resume painting from the very top, and didn’t want to smudge the lower half while doing so. The primary purpose behind the second coat was to perfect all the background illustrations. It hence focused on detailed shading to create smooth transitions from one hue to another without allowing individual brush strokes to show. <br/><br/>This time, I detailed Buddha’s facial features as well, creating the effect of light coming from within by highlighting his central facial section. And thus, with the completion of the second coat, I perfected my painting’s background. <br/><br/>The process of waiting continued once again. </div>} images={<><img className={classes.image} src="/assets/projects/circle-of-light/circle-of-light-zoom-1.jpg" style={{width: '25%'}}/><img className={classes.image} src="/assets/projects/circle-of-light/circle-of-light-zoom-2.jpg" style={{width: '25%'}}/><img className={classes.image} src="/assets/projects/circle-of-light/circle-of-light-zoom-3.jpg" style={{width: '43.5%'}}/></>}/>
+
+            <ContentItemText text={<div>I then began work on my third coat, focused on perfecting and finely detailing the portrait’s main object - the Buddha’s face. This was by far the most difficult aspect of the painting - especially the eyes - as I had to be doubly careful to ensure that the features radiated serenity while also ensuring that my brush strokes did, at no point, touch the surrounding background.<br/><br/> Only after painting this layer and allowing it to dry completely was I able to add the final finishing touches to the painting. I ensured that not only was the border between the Buddha and the background sharp, but also painted the sides of the canvas with a dark bluish-black paint to cover any strokes that went astray down the sides. <br/><br/>To mark my painting’s completion, I then used a thin brush to paint my name in the right-hand corner in white.</div>}/>
+        </Grid>
+        </>
     )
 }
--- pages/tech/metta-capital.js
+++ pages/tech/metta-capital.js
@@ -1,38 +1,87 @@
 import React from 'react';
-import { Grid, Typography, Hidden, Tooltip } from '@material-ui/core';
+import { Grid, Divider, Typography, Hidden, Tooltip } from '@material-ui/core';
+import { makeStyles } from '@material-ui/core/styles'
+// Components
+import { Cover } from '../../components/Projects/Cover'
+import { Intro } from '../../components/Projects/Intro'
+import { ProblemStatement } from '../../components/Projects/Problem_Statement'
 
-const font = 'Segoe UI';
+const primary = '#9E0D1B';
+const primary_25 = '#E6C2C6';
+const secondary = '#F23838';
+const tertiary = '#FFDCD0';
 
+const useStyles = makeStyles((theme) => ({
+    container: {
+        padding: '0 5vw',
+    },
+    divider: {
+        margin: '5vh 5vw 7.5vh',
+        display: 'flex',
+        width: '90vw',
+        background: primary_25,
+        [theme.breakpoints.down('xs')]: {
+            margin: '5vh 5vw 5vh',
+        },
+    },
+    embed: {
+        width: '100%',
+        height: '75vh', 
+        border: '3px solid #9E0D1B',
+        margin: '0 0 10vh 0',
+    },
+    title: {
+        color: primary,
+        margin: '0 0 4vh 0',
+    },
+    text: {
+        color: primary,
+        margin: '0 0 2vh 0',
+    }
+}));
+
+function ListItem(props) {
+    const classes = useStyles();
+
+    return(
+        <>
+        <Grid container className={classes.text}>
+            <Grid item xs={1}>
+                <Typography variant="body1"><b>{props.step}.</b></Typography>
+            </Grid>
+            <Grid item xs={10}>
+                <Typography variant="body2">{props.text}</Typography>
+            </Grid>
+        </Grid>
+        </>
+    )
+}
 
 export default function Story() {
+    const classes = useStyles();
+
     return(
-        <div>
-            <Grid container direction="row" justify="center" style={{backgroundColor: 'white', border: '5px solid #efefef', outline: 'none', width: '70vw', 'maxHeight': '75vh', 'overflowY': 'auto', alignItems: 'center'}}>
-                <Grid container direction="row" justify="center">
-                <Grid item xs={10} style={{'paddingTop': '5vh'}}>
-                    <iframe src="https://mettacapital.in" title="Metta Capital Advisors LLP" allowFullScreen={true} style={{'width': '100%', height: '50vh', border: '3px solid black'}}/>
-                </Grid>
-                <Grid item xs={10} style={{display:'flex',flexDirection:'row',justifyContent:'left'}}>
-                    <a href="https://mettacapital.in" target="_blank" rel="noopener noreferrer" style={{cursor: 'pointer', textDecoration: 'none', color:'black'}}>
-                    <Typography variant="caption" style={{fontStyle:'italic'}}>click to open in a new tab (https://mettacapital.in) </Typography>
-                    </a>
-                </Grid>
-                </Grid>
-                <Grid container direction="row" justify="center">
-                <Grid item xs={10} style={{'paddingTop': '2vh', 'paddingBottom': '5vh'}}>
-                    <Typography id="story-modal" variant="caption" style={{fontFamily: font}}>
-                    print( "MettaCapital is a boutique investment bank. I specialised as the sole web-developer, using the ReactJS library with a Material UI framework to develop their website from the ground up." ) <br/>
-                    <br/>
-                    Features: Navigation bar changes color based on scroll position (homepage), three different designs for persona modals to re nder design as responsive (team page), transactions slidebar using arrays (transactions page). Initially used a Google Maps API to run on the contact page, but public github hosting changed that.
-                    <br/> <br/>
-                    Tech Stack: ReactJs library, Material UI framework. <br/>
-                    APIs: 'window-scroll-position' (used to style navigation bar on homepage) <br/>
-                    Hosted on Github Pages, custom domain <br/>
-                    Created: May-June 2020
-                    </Typography>
-                </Grid>
-                </Grid>
+        <>
+        <Cover project_name="Metta Capital" bg_color={tertiary} title_color={secondary}></Cover>
+        <Grid container className={classes.container}>
+            <Intro title_color={primary_25} text_color={primary} company={"Metta Capital Advisors LLP"} role={"UI / UX Designer, Web Developer"} year={"May-June 2020"} category={<div>Tech<br/></div>} team={"Independent Project"} tools={<div><u>Software:</u> React.Js, Hosted on Github Pages, Domain via GoDaddy <br/><u>UI / UX:</u> Figma, Material UI</div>} tools1={<div><u>Software:</u> <br/>React.Js <br/>Hosted on Github Pages <br/>Domain via GoDaddy <br/></div>} tools2={<div><u>UI / UX:</u> <br/>Figma <br/>Material UI <br/></div>} introduction={<div>Metta Capital Advisors LLP is a boutique investment bank in India. <br/>I worked closely with the CEO to design and develop the company’s website from scratch. The website is also fully responsive.<br/>Available on <a href="https://mettacapital.in" target="_blank" rel="noopener noreferrer"><u>https://mettacapital.in</u></a></div>}/>
+        </Grid>
+
+        <Divider className={classes.divider}/>
+
+        <Grid container direction="column" className={classes.container}>
+            <Grid item xs={12}>
+                <iframe src="https://mettacapital.in" title="Metta Capital Advisors LLP" allowFullScreen={true} className={classes.embed}/>
             </Grid>
-        </div>
-    )
+            <Typography variant="h5" className={classes.title}>
+                Notable Features
+            </Typography>
+            
+            <ListItem step="1" text="The website is fully responsive -- three different designs were created for the team modals to adapt to the standard mobile phone, tablet, and laptop/desktop screen sizes"/>
+            <ListItem step="2" text="The color of the navigation bar (on the homepage) is modified depending on scroll position"/>
+            <ListItem step="3" text="The transactions gallery is updated via arrays"/>
+            <ListItem step="4" text="The Google Maps API was initially integrated with the contact page, but public github hosting required the key to be removed from the repository."/>
+        </Grid>
+        </>
+    )         
 }
