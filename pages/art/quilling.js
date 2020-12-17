3933ae5fc7ae45c930b3869bd95d96e9ad9549d4
yjaju <yjaju@usc.edu>
2020-12-17 18:36:30 +0530

    quilling

--- pages/art/quilling.js
+++ pages/art/quilling.js
@@ -1,109 +1,233 @@
 import React from 'react';
-import { Grid, Typography } from '@material-ui/core';
-import { KeyboardArrowLeftOutlined, KeyboardArrowRightOutlined } from '@material-ui/icons';
+import { Grid, Divider, Typography, Hidden, Tooltip } from '@material-ui/core';
+import { makeStyles } from '@material-ui/core/styles'
+// Components
+import { Cover } from '../../components/Projects/Cover'
+import { Intro } from '../../components/Projects/Intro'
+import { ProblemStatement } from '../../components/Projects/Problem_Statement'
 
-const font = 'Segoe UI';
+const primary = '#206CA3';
+const primary_25 = '#B0D9F7';
+const secondary = '#298FD7';
+const tertiary = '#CFF4D2';
+
+const useStyles = makeStyles((theme) => ({
+    container: {
+        padding: '0 5vw',
+    },
+    divider: {
+        margin: '5vh 5vw 7.5vh',
+        display: 'flex',
+        width: '90vw',
+        background: primary_25,
+        [theme.breakpoints.down('xs')]: {
+            margin: '5vh 5vw 5vh',
+        },
+    },
+    title: {
+        color: secondary,
+        margin: '0 0 1vh',
+    },
+    text: {
+        color: primary,
+        margin: '0 0 2vh',
+        [theme.breakpoints.down('xs')]: {
+          textAlign: 'center',
+      },
+    },
+    image: {
+      width: '100%',
+      height: 'auto',
+      margin: '0 0 2vh 0',
+      border: '1.5px solid #000000',
+      borderColor: primary,
+      borderRadius: '20px',
+    }
+}));
 
 
 export default function Story() {
-    const [quillingSrc, setQuillingSrc] = React.useState("/assets/quilling1.jpg");
+  const classes = useStyles();
+  
+  return(
+    <>
+      <Cover project_name="Quiilling" bg_color={tertiary} title_color={secondary}></Cover>
+      <Grid container className={classes.container}>
+          <Intro title_color={primary_25} text_color={primary} company={"N/A"} role={"Artist"} year={"2013-2016"} category={<div>Traditional Art</div>} team={"Independent Project"} tools={<div>Quilling Needle, Quilling Paper</div>} tools1={<div>Quilling Needle <br/> Quilling Paper</div>} tools2={<div></div>} introduction={<div>Quilling is an art of paper filigree in which strips of paper are rolled using a slitted-tool to create coils of paper. These coils are then shaped as desired, and sealed by glueing the loose end of paper. Put together, beautiful inticrate designs can be made. <br/>I designed and sold envelopes, cards, and boxes, amongst other traditional gift items, at boutique exhibitions in India for a few years.</div>}/>
+      </Grid>
 
-    function handleQuilling(props) {
-        if (props === "next") {
-          if (quillingSrc === "/assets/quilling1.jpg") {
-            setQuillingSrc("/assets/quilling2.jpg");
-          }
-          else if (quillingSrc === "/assets/quilling2.jpg") {
-            setQuillingSrc("/assets/quilling3.jpg");
-          }
-          else if (quillingSrc === "/assets/quilling3.jpg") {
-            setQuillingSrc("/assets/quilling4.jpg");
-          }
-          else if (quillingSrc === "/assets/quilling4.jpg") {
-            setQuillingSrc("/assets/quilling1.jpg");
-          }
-        }
-        if (props === "prev") {
-          if (quillingSrc === "/assets/quilling1.jpg") {
-            setQuillingSrc("/assets/quilling4.jpg");
-          }
-          else if (quillingSrc === "/assets/quilling2.jpg") {
-            setQuillingSrc("/assets/quilling1.jpg");
-          }
-          else if (quillingSrc === "/assets/quilling3.jpg") {
-            setQuillingSrc("/assets/quilling2.jpg");
-          }
-          else if (quillingSrc === "/assets/quilling4.jpg") {
-            setQuillingSrc("/assets/quilling3.jpg");
-          }
-        }
-      }
-    
+      <Divider className={classes.divider}/>
 
-    return(
-        <div>
-          <Grid container direction="row" justify="center" style={{backgroundColor: 'white', border: '5px solid #efefef', outline: 'none', width: '70vw', 'maxHeight': '75vh', 'overflowY': 'auto', alignItems: 'center', 'paddingBottom': '5vh', 'paddingTop': '5vh'}}>
-            <Grid container direction="column" justify="center" alignItems="center">
-              <Grid item xs={10} style={{marginTop: '1vh'}}>
-                <Grid container direction="row" justify="center" style={{alignItems: 'center'}}>
-                  <Grid item xs={1}  style={{display: 'flex', justifyContent: 'center', alignItems: 'center'}}>
-                    <KeyboardArrowLeftOutlined id="leftPtr" fontSize="small" onClick={()=>{handleQuilling("prev")}} style={{cursor: 'pointer'}}/>
-                  </Grid>
-                  <Grid item xs={10} style={{display:'flex', flexDirection: 'column', alignItems:'center'}}>
-                    <img id="Quilling" src={quillingSrc} alt="Quilling" title="Quilling" style={{'width': '98%', paddingBottom: '1vh'}}/>
-                  </Grid>
-                  <Grid item xs={1}  style={{display: 'flex', justifyContent: 'center', alignItems: 'center'}}>
-                    <KeyboardArrowRightOutlined id="rightPtr" fontSize="small" onClick={()=>{handleQuilling("next")}} style={{cursor: 'pointer'}}/>
-                  </Grid>
-                </Grid>
-              </Grid>
-              <Grid item xs={8} style={{marginTop: '1vh', display: 'flex', justifyContent: 'center', alignItems: 'center'}}>
-                <Typography id="story-modal" variant="caption" style={{fontFamily: font}}>
-                  The Puppet Show — <br/>
-                  <br/>
-                  The strip of paper <br/>
-                  Is a puppet <br/>
-                  And the tool <br/>
-                  The string <br/>
-                  <br/>
-                  The string twists <br/>
-                  And the puppet rotates <br/>
-                  The strip becomes <br/>
-                  A tight coil <br/>
-                  <br/>
-                  The strings are cut <br/>
-                  The puppet stretches <br/>
-                  The coil becomes <br/>
-                  Loose <br/>
-                  <br/>
-                  But <br/>
-                  I am there <br/>
-                  The puppeteer <br/>
-                  <br/>
-                  I pinch the coil <br/>
-                  Bend it to my will <br/>
-                  The loose coil becomes <br/>
-                  A teardrop <br/>
-                  A dimaond <br/>
-                  A victim of my imagination <br/>
-                  <br/>
-                  Glued in place <br/>
-                  All the puppets <br/>
-                  Together <br/>
-                  Become <br/>
-                  The Puppet Show <br/>
-                  --
-                  <br/> <br/>
-                  Quilling is an art of paper filigree in which strips of paper are rolled using a slitted-tool to create coils of paper. These coils are then shaped as desired, and sealed by glueing the loose end of paper. Put together, beautiful inticrate designs can be made. <br/> <br/>
-                  I sold envelopes, cards, boxes, etc for Rs. 100-200 ($ 2-3) at exhibitions for a few years.
-                  <br/> <br/>
-                  Created: 2013-2016 <br/>
-                  <br/>
+      {/* <Grid container className={classes.container} justify="space-between">
+        <Grid item xs={3}>
+          <Typography variant="body2" className={classes.text}>
+            <b>The Puppet Show — </b><br/>
+            <br/>
+            The strip of paper <br/>
+            Is a puppet <br/>
+            And the tool <br/>
+            The string <br/>
+            <br/>
+            The string twists <br/>
+            And the puppet rotates <br/>
+            The strip becomes <br/>
+            A tight coil <br/>
+            <br/>
+            The strings are cut <br/>
+            The puppet stretches <br/>
+            The coil becomes <br/>
+            Loose <br/>
+            <br/>
+            But <br/>
+            I am there <br/>
+            The puppeteer <br/>
+            <br/>
+            I pinch the coil <br/>
+            Bend it to my will <br/>
+            The loose coil becomes <br/>
+            A teardrop <br/>
+            A dimaond <br/>
+            A victim of my imagination <br/>
+            <br/>
+            Glued in place <br/>
+            All the puppets <br/>
+            Together <br/>
+            Become <br/>
+            The Puppet Show <br/>
+            --
+          </Typography>
+        </Grid>
+        <Grid item xs={4} container direction="column">
+          <img src="/assets/projects/quilling/quilling_1.jpg" className={classes.image}/>
+          <img src="/assets/projects/quilling/quilling_4.jpg" className={classes.image}/>
+          <img src="/assets/projects/quilling/quilling_6.jpg" className={classes.image}/>
+        </Grid> 
+        <Grid item xs={4} container direction="column">
+          <img src="/assets/projects/quilling/quilling_5.jpg" className={classes.image}/>
+          <img src="/assets/projects/quilling/quilling_3.jpg" className={classes.image}/>
+          <img src="/assets/projects/quilling/quilling_9.jpg" className={classes.image}/>
+        </Grid> 
+      </Grid> */}
 
-                </Typography>
-              </Grid>
-            </Grid>
-          </Grid>
-        </div>
-    )
+      <Grid container className={classes.container} justify="space-between">
+        <Grid item xs={12}>
+          <Typography variant="body1" className={classes.title}>
+          <b>The Puppet Show — </b><br/>
+          </Typography>
+        </Grid>
+        <Grid item xs={12} sm={3}>
+          <Typography variant="body2" align="right" className={classes.text}>
+            The strip of paper <br/>
+            Is a puppet <br/>
+            And the tool <br/>
+            The string <br/>
+            <br/>
+            The string twists <br/>
+            And the puppet rotates <br/>
+            The strip becomes <br/>
+            A tight coil <br/>
+            <br/>
+            <img src="/assets/projects/quilling/quilling_7.jpg" className={classes.image}/>
+            <img src="/assets/projects/quilling/quilling_4.jpg" className={classes.image}/>
+          </Typography>
+        </Grid>
+        <Grid item xs={12} sm={3} container direction="column">
+          <img src="/assets/projects/quilling/quilling_1.jpg" className={classes.image}/>
+          <img src="/assets/projects/quilling/quilling_3.jpg" className={classes.image}/>
+          <Typography variant="body2" className={classes.text} align="center">
+            The strings are cut <br/>
+            The puppet stretches <br/>
+            The coil becomes <br/>
+            Loose <br/>
+            <br/>
+            But <br/>
+            I am there <br/>
+            The puppeteer <br/>
+          </Typography>
+          <img src="/assets/projects/quilling/quilling_6.jpg" className={classes.image}/>
+        </Grid> 
+        <Grid item xs={12} sm={3} container direction="column">
+          <img src="/assets/projects/quilling/quilling_5.jpg" className={classes.image}/>
+          <img src="/assets/projects/quilling/quilling_9.jpg" className={classes.image}/>
+          <Typography variant="body2" className={classes.text}>
+            I pinch the coil <br/>
+            Bend it to my will <br/>
+            The loose coil becomes <br/>
+            A teardrop <br/>
+            A dimaond <br/>
+            A victim of my imagination <br/>
+            <br/>
+            Glued in place <br/>
+            All the puppets <br/>
+            Together <br/>
+            Become <br/>
+            The Puppet Show <br/>
+            --
+          </Typography>
+        </Grid> 
+      </Grid>
+
+      {/* <Grid container className={classes.container} justify="space-between">
+        <Grid item xs={12}>
+          <Typography variant="body1" className={classes.title}>
+          <b>The Puppet Show — </b><br/>
+          </Typography>
+        </Grid>
+        <Grid item xs={3}>
+          <Typography variant="body2" className={classes.text} align="right">
+            The strip of paper <br/>
+            Is a puppet <br/>
+            And the tool <br/>
+            The string <br/>
+            <br/>
+            The string twists <br/>
+            And the puppet rotates <br/>
+            The strip becomes <br/>
+            A tight coil <br/>
+            Unable to move <br/>
+            As if <br/>
+            Chained <br/>
+            <br/>
+            <img src="/assets/projects/quilling/quilling_7.jpg" className={classes.image}/>
+            <img src="/assets/projects/quilling/quilling_4.jpg" className={classes.image}/>
+          </Typography>
+        </Grid>
+        <Grid item xs={3} container direction="column">
+          <Typography variant="body2" className={classes.text} align="center">
+            <br/> <br/>
+            The strings are cut <br/>
+            The puppet stretches <br/>
+            The coil becomes <br/>
+            Loose <br/>
+            <br/>
+            But <br/>
+            I am there <br/>
+            The puppeteer <br/> <br/> <br/>
+          </Typography>
+          <img src="/assets/projects/quilling/quilling_1.jpg" className={classes.image}/>
+          <img src="/assets/projects/quilling/quilling_6.jpg" className={classes.image}/>
+          <img src="/assets/projects/quilling/quilling_3.jpg" className={classes.image}/>
+        </Grid> 
+        <Grid item xs={3} container direction="column">
+          <Typography variant="body2" className={classes.text}>
+            I pinch the coil <br/>
+            Bend it to my will <br/>
+            The loose coil becomes <br/>
+            A teardrop <br/>
+            A dimaond <br/>
+            A victim of my imagination <br/>
+            <br/>
+            Glued in place <br/>
+            All the puppets <br/>
+            Together <br/>
+            Become <br/>
+            The Puppet Show <br/>
+          </Typography>
+          <img src="/assets/projects/quilling/quilling_5.jpg" className={classes.image}/>
+          <img src="/assets/projects/quilling/quilling_9.jpg" className={classes.image}/>
+          
+        </Grid> 
+      </Grid> */}
+    </>
+  )
 }
