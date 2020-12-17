3bcd83256ee447138f1261b142e38e6366d6f477
yjaju <yjaju@usc.edu>
2020-12-17 18:59:33 +0530

    favicon & title

--- pages/_app.js
+++ pages/_app.js
@@ -9,9 +9,9 @@ function MyApp({ Component, pageProps }) {
       <Head>
         <title>Yashvi Jaju</title>
         <link rel="icon" href="/favicon.ico" />
-        <meta name="author" content="Yashvi Jaju">
-        <meta name="keywords" content="Portfolio, Computer Science Business Administration, Yashvi Jaju">
-        <meta name="description" content="Yashvi Jaju is pursuing Computer Science and Business Administration at the University of Southern California, with a minor in Entrepreneurship. Over the years, she has founded startups, developed software products, and created digital and traditional art">
+        <meta name="author" content="Yashvi Jaju"/>
+        <meta name="keywords" content="Portfolio, Computer Science Business Administration, Yashvi Jaju"/>
+        <meta name="description" content="Yashvi Jaju is pursuing Computer Science and Business Administration at the University of Southern California, with a minor in Entrepreneurship. Over the years, she has founded startups, developed software products, and created digital and traditional art"/>
       </Head>
       <NavBar/>
       <Component {...pageProps} />
